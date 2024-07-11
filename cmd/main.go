e07ef6b397a3b58955d969a97b299e6d1c32727c
yermanovberik <119285164+yermanovberik@users.noreply.github.com>
2024-07-11 11:04:31 +0500

    update url

--- cmd/main.go
+++ cmd/main.go
@@ -8,13 +8,12 @@ import (
 )
 
 func main() {
-
 	router := mux.NewRouter()
-	router.HandleFunc("/", handler.HandleFunc)
+	router.HandleFunc("/proxy", handler.HandleFunc)
 
-	err := http.ListenAndServe(":8080", router)
 	log.Println("Starting server on :8080")
 
+	err := http.ListenAndServe(":8080", router)
 	if err != nil {
 		log.Fatal(err)
 	}
