da37e4796ef0effc2bea92cec86423a696b26e68
yermanovberik <119285164+yermanovberik@users.noreply.github.com>
2024-07-11 10:05:16 +0500

    add render.yaml

--- cmd/main.go
+++ cmd/main.go
@@ -13,10 +13,9 @@ func main() {
 	router.HandleFunc("/", handler.HandleFunc)
 
 	err := http.ListenAndServe(":8080", router)
+	log.Println("Starting server on :8080")
 
 	if err != nil {
 		log.Fatal(err)
 	}
-	log.Println("Starting server on :8080")
-
 }
